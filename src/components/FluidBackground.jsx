ea053f7e0a1e683b4b38e4acaa7ec18de9bb1fbc
Wellington-Andrade <wellingtonevangelista28@gmail.com>
2026-06-26 11:15:41 -0300

    Refine fluid background motion

--- src/components/FluidBackground.jsx
+++ src/components/FluidBackground.jsx
@@ -25,18 +25,58 @@ export default function FluidBackground() {
         invalidateOnRefresh: true,
       });
 
+      gsap.to(".fluid-blob-inner-1", {
+        x: 36,
+        y: -24,
+        scale: 1.08,
+        duration: 9,
+        repeat: -1,
+        yoyo: true,
+        ease: "sine.inOut",
+      });
+
+      gsap.to(".fluid-blob-inner-2", {
+        x: -34,
+        y: 30,
+        scale: 1.12,
+        duration: 11,
+        repeat: -1,
+        yoyo: true,
+        ease: "sine.inOut",
+      });
+
+      gsap.to(".fluid-blob-inner-3", {
+        x: 26,
+        y: 34,
+        scale: 1.1,
+        duration: 13,
+        repeat: -1,
+        yoyo: true,
+        ease: "sine.inOut",
+      });
+
+      gsap.to(".fluid-blob-inner-4", {
+        x: -30,
+        y: -22,
+        scale: 1.07,
+        duration: 10,
+        repeat: -1,
+        yoyo: true,
+        ease: "sine.inOut",
+      });
+
       gsap.to(".fluid-blob-1", {
-        x: 220,
+        x: 280,
         y: 120,
-        scale: 1.18,
-        opacity: 0.72,
+        scale: 1.16,
+        opacity: 0.44,
         ease: "none",
         scrollTrigger: scroll(),
       });
 
       gsap.to(".fluid-blob-2", {
-        x: -260,
-        y: 150,
+        x: -320,
+        y: 160,
         scale: 1.28,
         opacity: 0.56,
         ease: "none",
@@ -44,19 +84,25 @@ export default function FluidBackground() {
       });
 
       gsap.to(".fluid-blob-3", {
-        x: 120,
-        y: -240,
+        x: 150,
+        y: -220,
         scale: 1.34,
-        opacity: 0.62,
+        opacity: 0.48,
         ease: "none",
         scrollTrigger: scroll(),
       });
 
       gsap.to(".fluid-blob-4", {
-        x: -150,
-        y: -130,
-        scale: 1.16,
-        opacity: 0.5,
+        x: -190,
+        y: -150,
+        scale: 1.2,
+        opacity: 0.46,
+        ease: "none",
+        scrollTrigger: scroll(),
+      });
+
+      gsap.to(rootRef.current, {
+        filter: "hue-rotate(12deg) saturate(1.06) brightness(1.02)",
         ease: "none",
         scrollTrigger: scroll(),
       });
@@ -67,10 +113,18 @@ export default function FluidBackground() {
 
   return (
     <div ref={rootRef} className="fluid-bg" aria-hidden="true">
-      <div className="fluid-blob fluid-blob-1" />
-      <div className="fluid-blob fluid-blob-2" />
-      <div className="fluid-blob fluid-blob-3" />
-      <div className="fluid-blob fluid-blob-4" />
+      <div className="fluid-blob fluid-blob-1">
+        <div className="fluid-blob-inner fluid-blob-inner-1" />
+      </div>
+      <div className="fluid-blob fluid-blob-2">
+        <div className="fluid-blob-inner fluid-blob-inner-2" />
+      </div>
+      <div className="fluid-blob fluid-blob-3">
+        <div className="fluid-blob-inner fluid-blob-inner-3" />
+      </div>
+      <div className="fluid-blob fluid-blob-4">
+        <div className="fluid-blob-inner fluid-blob-inner-4" />
+      </div>
       <div className="fluid-noise" />
     </div>
   );
